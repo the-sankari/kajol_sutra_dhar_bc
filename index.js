fcec14f0f32c067e2aea5d8d36a0fdd659de6885
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-04-01 02:04:25 +0300

    nav menu js transfered ti nav.js

--- index.js
+++ index.js
@@ -62,3 +62,5 @@ cardElements.forEach((card, index) => {
     }
   });
 });
+
+
--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,22 @@
-const links = document.querySelectorAll('nav ul li a');
+const links = document.querySelectorAll("nav ul li a");
 
-links.forEach(link => {
-  link.addEventListener('click', function(e) {
+links.forEach((link) => {
+  link.addEventListener("click", function (e) {
     e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
+    const target = document.querySelector(this.getAttribute("href"));
     target.scrollIntoView({ behavior: "smooth" });
   });
+});
+
+// JavaScript for mobile responsive navbar
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const menuItems = document.querySelector(".menu-items");
+
+  menuToggle.addEventListener("click", function () {
+    menuItems.classList.toggle("active");
+  });
+  // current year
+  var currentYear = new Date().getFullYear();
+  document.getElementById("currentYear").textContent = currentYear;
 });
